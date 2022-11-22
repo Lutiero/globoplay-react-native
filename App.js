d49c4a80b4b6264715bac78e66b7e99fb264235a
Lutiero <lutiero@gmail.com>
2022-11-22 16:14:45 -0400

    sabe la deus, demorei pra commitar

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/Home';
 
 export default function App() {
+  const getMoviesData = async () => {
+    const api_key = 'ae440b562b9d6bd3a103981d15544077';
+    const api = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
+
+    const myRequest = await fetch(api, {
+      method: 'GET'
+    });
+
+    const response = await myRequest.json();
+    console.log(response);
+
+    let tranformado = response.results.map((movie) => {
+      return {
+        original_title: movie.original_title
+      }
+    });
+
+    console.log(tranformado);
+
+  }
+
+  getMoviesData();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
